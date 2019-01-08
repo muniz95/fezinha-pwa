2881196b3c4b7682bd3897cfe54f17d716880abc
Rodrigo Muniz <muniz95@bsd.com.br>
2019-01-08 15:51:08 +0000

    Barra de opções de jogos

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -5,11 +5,13 @@ const Home = () => (
 	<div class={style.home}>
 		<p>Selecione o jogo:</p>
 		<div class={style.gameRow}>
-			<div class={style.gameCircle}>1</div>
-			<div class={style.gameCircle}>2</div>
-			<div class={style.gameCircle}>3</div>
-			<div class={style.gameCircle}>4</div>
-			<div class={style.gameCircle}>5</div>
+			<div class={[style.gameCircle, style.megasena].join(' ')}>1</div>
+			<div class={[style.gameCircle, style.lotofacil].join(' ')}>2</div>
+			<div class={[style.gameCircle, style.quina].join(' ')}>3</div>
+			<div class={[style.gameCircle, style.lotomania].join(' ')}>4</div>
+			<div class={[style.gameCircle, style.timemania].join(' ')}>5</div>
+			<div class={[style.gameCircle, style.duplasena].join(' ')}>6</div>
+			<div class={[style.gameCircle, style.diaDeSorte].join(' ')}>7</div>
 		</div>
 	</div>
 );
