86047fc52292373b1c713260d4a0585d937492c5
Rodrigo Muniz <muniz95@bsd.com.br>
2019-02-01 17:26:54 +0000

    Layout da tela de opções de jogo concluído

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -5,13 +5,13 @@ const Home = () => (
 	<div class={style.home}>
 		<p>Selecione o jogo:</p>
 		<div class={style.gameRow}>
-			<div class={[style.gameCircle, style.megasena].join(' ')}>1</div>
-			<div class={[style.gameCircle, style.lotofacil].join(' ')}>2</div>
-			<div class={[style.gameCircle, style.quina].join(' ')}>3</div>
-			<div class={[style.gameCircle, style.lotomania].join(' ')}>4</div>
-			<div class={[style.gameCircle, style.timemania].join(' ')}>5</div>
-			<div class={[style.gameCircle, style.duplasena].join(' ')}>6</div>
-			<div class={[style.gameCircle, style.diaDeSorte].join(' ')}>7</div>
+			<div class={[style.gameCircle, style.megasena].join(' ')}>Mega Sena</div>
+			<div class={[style.gameCircle, style.lotofacil].join(' ')}>Lotofácil</div>
+			<div class={[style.gameCircle, style.quina].join(' ')}>Quina</div>
+			<div class={[style.gameCircle, style.lotomania].join(' ')}>Lotomania</div>
+			<div class={[style.gameCircle, style.timemania].join(' ')}>Timemania</div>
+			<div class={[style.gameCircle, style.duplasena].join(' ')}>Dupla Sena</div>
+			<div class={[style.gameCircle, style.diaDeSorte].join(' ')}>Dia de Sorte</div>
 		</div>
 	</div>
 );
