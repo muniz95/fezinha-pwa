b5c04f8d52b1f8dcb2aaffa31be4926f2cadc905
Rodrigo Muniz <muniz95@bsd.com.br>
2019-02-07 18:48:39 +0000

    Geração de números para Mega Sena e Loto Fácil funcionando

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -9,43 +9,44 @@ class Home extends Component {
 			selectedGame: ''
 		}
 		
+		this.createResultsPanel = this.createResultsPanel.bind(this);
 		this.generateNumbers = this.generateNumbers.bind(this);
 		this.setGame = this.setGame.bind(this);
 	}
 
 	render() {
-		const divSelectedGame = this.generateNumbers(this.state.selectedGame);
+		const divSelectedGame = this.createResultsPanel(this.state.selectedGame);
 		return (
 			<div class={style.home}>
 				<div class={style.title}>
 					<p>Selecione o jogo:</p>
 				</div>
 				<div class={style.gameRow}>
-					<div class={[style.gameCircle, style.megasena].join(' ')}
+					<div class={[style.gameCircle, style.megasena, style.lightFont].join(' ')}
 						onClick={() => this.setGame('megasena')}>
 						Mega Sena
 					</div>
-					<div class={[style.gameCircle, style.lotofacil].join(' ')}
+					<div class={[style.gameCircle, style.lotofacil, style.lightFont].join(' ')}
 						onClick={() => this.setGame('lotofacil')}>
 						Lotofácil
 					</div>
-					<div class={[style.gameCircle, style.quina].join(' ')}
+					<div class={[style.gameCircle, style.quina, style.lightFont].join(' ')}
 						onClick={() => this.setGame('quina')}>
 						Quina
 					</div>
-					<div class={[style.gameCircle, style.lotomania].join(' ')}
+					<div class={[style.gameCircle, style.lotomania, style.lightFont].join(' ')}
 						onClick={() => this.setGame('lotomania')}>
 						Lotomania
 					</div>
-					<div class={[style.gameCircle, style.timemania].join(' ')}
+					<div class={[style.gameCircle, style.timemania, style.darkFont].join(' ')}
 						onClick={() => this.setGame('timemania')}>
 						Timemania
 					</div>
-					<div class={[style.gameCircle, style.duplasena].join(' ')}
+					<div class={[style.gameCircle, style.duplasena, style.lightFont].join(' ')}
 						onClick={() => this.setGame('duplasena')}>
 						Dupla Sena
 					</div>
-					<div class={[style.gameCircle, style.diaDeSorte].join(' ')}
+					<div class={[style.gameCircle, style.diaDeSorte, style.lightFont].join(' ')}
 						onClick={() => this.setGame('diaDeSorte')}>
 						Dia de Sorte
 					</div>
@@ -55,50 +56,57 @@ class Home extends Component {
 		);
 	}
 	
-	generateNumbers(selectedGame) {
-		let divSelectedGame
-		switch (selectedGame) {
-			case 'megasena':
-				divSelectedGame = <div class={style.numbersRow}>
-					<div>01 - 02 - 03 - 04 - 05 - 06</div>
-				</div>;
-				break;
-			case 'lotofacil':
-				divSelectedGame = <div class={style.numbersRow}>
-					<div>
-						01 - 02 - 03 - 04 - 05 - 06 - 01 - 02 - 03 - 04 - 05 - 06 - 04 - 05 - 06
-					</div>
-				</div>;
-				break;
-			case 'quina':
-				divSelectedGame = <div class={style.numbersRow}>
-					<div>01 - 02 - 03 - 04 - 05 - 06</div>
-				</div>;
-				break;
-			case 'lotomania':
-				divSelectedGame = <div class={style.numbersRow}>
-					<div>01 - 02 - 03 - 04 - 05 - 06</div>
-				</div>;
-				break;
-			case 'timemania':
-				divSelectedGame = <div class={style.numbersRow}>
-					<div>01 - 02 - 03 - 04 - 05 - 06</div>
-				</div>;
-				break;
-			case 'duplasena':
-				divSelectedGame = <div class={style.numbersRow}>
-					<div>01 - 02 - 03 - 04 - 05 - 06</div>
-				</div>;
-				break;
-			case 'diaDeSorte':
-				divSelectedGame = <div class={style.numbersRow}>
-					<div>01 - 02 - 03 - 04 - 05 - 06 - 07</div>
-				</div>;
-				break;
-			default:
-				divSelectedGame = null;
-		}
+	createResultsPanel(selectedGame) {
+		const numbers = this.generateNumbers(selectedGame);
+		const divSelectedGame = (
+			<div class={style.numbersRow}>
+				<div>{numbers.join(' - ')}</div>
+			</div>
+		);
 		return divSelectedGame;
+	}
+	
+	generateNumbers(selectedGame) {
+		const gameNumbersQuantity = {
+			'megasena': {
+				quantity: 6,
+				range: 60
+			},
+			'lotofacil': {
+				quantity: 15,
+				range: 30
+			},
+			'quina': {
+				quantity: 6,
+				range: 30
+			},
+			'lotomania': {
+				quantity: 6,
+				range: 30
+			},
+			'timemania': {
+				quantity: 6,
+				range: 30
+			},
+			'duplasena': {
+				quantity: 6,
+				range: 30
+			},
+			'diaDeSorte': {
+				quantity: 6,
+				range: 30
+			}
+		}
+		const numbers = new Set();
+		const sortCriteria = (a, b) => a - b;
+		
+		if(selectedGame) {
+			while(numbers.size < gameNumbersQuantity[selectedGame].quantity) {
+				numbers.add(Math.floor(Math.random() * gameNumbersQuantity[selectedGame].range) + 1);
+			}
+		}
+		
+		return Array.from(numbers).sort(sortCriteria);
 	}
 	
 	setGame(game) {
